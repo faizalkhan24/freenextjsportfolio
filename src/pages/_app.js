a6bf52c1335f8489088619d58b48fc47b39cc907
Faizal Khan <103817308+faizalkhan24@users.noreply.github.com>
2025-01-28 12:47:31 +0530

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,13 +2,17 @@ import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
 import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <meta name="google-adsense-account" content="ca-pub-8790655614720711" />
+      </Head>
       <Navbar />
       <Component {...pageProps} />
-      <Footer/>
+      <Footer />
     </>
   );
 }
